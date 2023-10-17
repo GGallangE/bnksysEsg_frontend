f093e5e7e9f1f2b1bceb3ac1a2a39dc865e1aabb
김도균 <kimdogyun@gimdogyun-ui-MacBookAir.local>
2023-10-17 23:10:57 +0900

    feat:first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  console.log('a')
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8080/main/boardid", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+        console.log(response)
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+        } else {
+          console.log("서버 응답 오류");
+        }
+      } catch (error) {
+        console.error("에러 발생: ", error);
+      }
+    }
+
+    fetchData();
+  }, []); // useEffect가 컴포넌트가 렌더링될 때 한 번만 실행되도록 빈 배열을 전달합니다.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 여기에 내용을 렌더링할 수 있습니다 */}
     </div>
   );
 }
