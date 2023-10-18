e0aae6728e437e193a5f800d11b80908a3b04872
GGallangE <ehrbs4789@gmail.com>
2023-10-18 14:16:13 +0900

    feat:get from springboot

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ function App() {
 
     useEffect(() => {
         axios.get('/main/boardid')
-        .then(response => setHello(response.data)
+        .then(response => {
+          console.log(response);
+          setHello(response.data.data)
+          console.log(hello)  
+        }
         )
         .catch(error => console.log(error))
     }, []);
 
+    const dataArray = Object.values(hello)
     return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
+      <div>
+        백엔드에서 가져온 데이터입니다:
+        <ul>
+            {dataArray.map((item, index) => (
+                <li key={index}>
+                    User ID: {item.userId}, User Name: {item.userName}, Board ID: {item.boardId}, Board Name: {item.boardName}
+                </li>
+            ))}
+        </ul>
+    </div>
     );
 }
 
