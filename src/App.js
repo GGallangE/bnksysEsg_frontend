471839400cfae777baf90b1b05cd865b226162c3
miiin-se <alstp7845@gmail.com>
2023-10-18 14:16:58 +0900

    feat: test

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-   const [hello, setHello] = useState('')
+   const [message, setMessage] = useState([]);
 
     useEffect(() => {
-        axios.get('/main/boardid')
-        .then(response => setHello(response.data)
-        )
-        .catch(error => console.log(error))
+        axios.get('/main/User')
+        .then((response)=>{
+          setMessage(response.data);
+        })
+        // .then((data)=>{
+        //     setMessage(response.data);
+        // })
     }, []);
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            백엔드에서 가져온 데이터입니다 : {message}
         </div>
     );
 }
