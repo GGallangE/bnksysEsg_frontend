cf8f82b2ae003028e1dce087271088b28eae77b6
miiin-se <alstp7845@gmail.com>
2023-10-18 13:42:57 +0900

    feat:create setupProxy

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import './App.css';
-import React, { useEffect } from 'react';
+// src/main/frontend/src/App.js
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  console.log('a')
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("http://localhost:8080/main/boardid", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-        console.log(response)
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-        } else {
-          console.log("서버 응답 오류");
-        }
-      } catch (error) {
-        console.error("에러 발생: ", error);
-      }
-    }
+   const [hello, setHello] = useState('')
 
-    fetchData();
-  }, []); // useEffect가 컴포넌트가 렌더링될 때 한 번만 실행되도록 빈 배열을 전달합니다.
+    useEffect(() => {
+        axios.get('/main/boardid')
+        .then(response => setHello(response.data)
+        )
+        .catch(error => console.log(error))
+    }, []);
 
-  return (
-    <div className="App">
-      {/* 여기에 내용을 렌더링할 수 있습니다 */}
-    </div>
-  );
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
 }
 
 export default App;
