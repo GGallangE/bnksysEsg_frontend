5e7ede751a880d9a5403594b30d138760a513b53
GGallangE <ehrbs4789@gmail.com>
2023-10-25 10:10:25 +0900

    feat:view

--- src/DetailApi_Status.js
+++ src/DetailApi_Status.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect ,useState} from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function DetailApi_Status() {
   const {apilistid} = useParams();
+  const [searchResults, setSearchResults] = useState({});
+
+  useEffect(() => {
+    const fetchApiDetails = async () => {
+      try {
+        const response = await axios.get(`/spring/connection/result/${apilistid}`)
+        setSearchResults(response.data.data[0]);
+      } catch (error) {
+        console.error("Error fetching API details: ", error);
+      }
+    };
+
+    // Call the API details fetch function when the component mounts
+    fetchApiDetails();
+  }, [apilistid]);
 
   return (
-    <h3>{apilistid} 맞습니다!</h3>
+    <div>
+      <h3>{searchResults.apiname} 맞습니다!</h3>
+      <div>
+        <p>API Name: {searchResults.apiname}</p>
+        <p>제공기관: {searchResults.prvorg}</p>
+        <p>조회수: {searchResults.view}</p>
+        <p>사용 횟수: {searchResults.nbruses}</p>
+        <p>API 설명: {searchResults.apiexpl}</p>
+      </div>
+    </div>
   );
 }
 
